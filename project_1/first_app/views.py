9bf8e919eac965ece5b978748f6a731ca71a72ba
Shakil4432 <shakil4432@gmail.com>
2024-03-05 17:21:48 +0600

    first commit

---
+++ project_1/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def courses(request):
+    return HttpResponse("this is first app/ courses page");
+def about(request):
+    return HttpResponse("this is first app/ about page");
+def home(request):
+    return HttpResponse("this is first app/ home page");
